13149c00bc7a152366ac83af66b4fe231ff37c59
piasun <77275966+piasun@users.noreply.github.com>
2024-07-25 16:56:34 +0200

    Added logIn page and functions resolve #1

---
+++ src/components/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { saveToken, saveUser } from "../utils/useLocalStorage";
+import { useNavigate } from "react-router-dom";
+import { LOGIN_URL } from "../constants/apiUrl";
+import fetchProfile from "./profiles/FetchProfile";
+ 
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(LOGIN_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      console.log("data response.json: ", data);
+ 
+      if (response.ok) {
+        saveToken(data.accessToken);
+        const profileData = await fetchProfile(data.name);
+        saveUser(profileData);
+        navigate("/Profile");
+      } else {
+        setError("Invalid email or password");
+      }
+    } catch (error) {
+      setError("Invalid email or password");
+    }
+  };
+ 
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+ 
+export default Login;
