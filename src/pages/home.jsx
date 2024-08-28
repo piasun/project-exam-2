77639783025052388536667f2cc8422a472d8123
piasun <77275966+piasun@users.noreply.github.com>
2024-08-28 13:33:08 +0200

    added pages

---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+ 
+const Home = () => {
+  return (
+    <>
+      <Container>
+      <div className="container mt-5">
+      <h1>Welcome to PostIt!</h1>
+      <p>This is a social media platform where you can interact, post, and share your thoughts. Get started by logging in or signing up!</p>
+    </div>
+        
+      </Container>
+    </>
+  );
+};
+ 
+export default Home;
